13e0b9a585f4e05441ef762b3d264f190b883f7b
siddhey7 <siddheythute7@gmail.com>
2026-05-30 17:36:03 +0530

    Removed duplicate js projects folder

--- js projects/digital clock/script.js
+++
@@ -1,7 +0,0 @@
-const Clock = document.querySelector("#clock");
-
-setInterval(function () {
-  let date = new Date();
-  //   console.log(date.toLocaleTimeString);
-  Clock.innerHTML = date.toLocaleTimeString();
-}, 1000);
